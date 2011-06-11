8513eebe3adaa565e961c0fb718b3ad14baedabe
shardul mohite <shardul@weboniselab.com>
2011-06-11 20:21:58 +0530

    first cut at an inefficient and experimental similar projects algorithm

--- app/models/project.rb
+++ app/models/project.rb
@@ -21,22 +21,35 @@ class Project
   embeds_many :documents
   belongs_to :user
 
-
   validates :title, :description, :address, :presence => true
-
   validates_associated :user
 
   geocoded_by :address
   after_validation :geocode
  
+  MIN_SIMILARITY_THRESHOLD = 0.7 
  
- def simular_project
-    GetProject p
-    keywords= p.title.split(" ") << p.tags
-     
-    listprojects=Project.find(:all, :conditions => ['title like % o tags'] )
-    
- end
+  def similar_projects
+    all_similar_projects = []
+    list_of_candidate_projects = Project.all # to be made more efficient 
+    list_of_candidate_projects.each {|candidate_project|
+      similarity = calculate_similarity_with candidate_project
+      all_similar_projects << candidate_project if similarity > MIN_SIMILARITY_THRESHOLD
+    }
+    all_similar_projects[0..4]
+  end
 
+  private
 
+  def keywords
+    # returns a project's keywords, which are basically keywords form the title union with it's tags
+    self.title.split(" ") << self.tags
+    #todo: remove common noise like 'The' or 'A' or 'And' or 'in' from the title since they are not really keywords
+  end
+  
+  def calculate_similarity_with other_project
+    other_keywords = other_project.keywords
+    similarity = (self.keywords & other_keywords).count.to_f / other_keywords.count
+  end
+  
 end
