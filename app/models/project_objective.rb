381611cf5a35119081595a00e508e34c3852a17d
Jiren Patel <jiren@joshsoftware.com>
2011-06-11 21:27:22 +0530

    Project Objective and Sub Objective

--- app/models/project_objective.rb
+++ app/models/project_objective.rb
@@ -3,5 +3,7 @@ class ProjectObjective
   field :name, :type => String
   field :objective_id
   belongs_to :project
-  has_many :sub_objectives, :class => :project_objective, :throught => :objective_id
+
+  belongs_to :project_objective, :class => 'ProjectObjective'
+  has_many :sub_objectives, :class_name => 'ProjectObjective', :foreign_key => 'objective_id'
 end
