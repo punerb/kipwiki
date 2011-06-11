6f5348f6fcae3cfdd93ce1ddbd85e72977f2c225
Gautam Rege <gautam@joshsoftware.com>
2011-06-11 19:16:35 +0530

    Re-structered project fields

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,27 +4,37 @@ class Project
 
   field :title, :type => String
   field :description, :type => String
-  field :address, :type => String
   field :view_count, :type => Integer 
   field :vote_count, :type => Integer
-  field :coordinates, :type => Array
+  field :address, :type => String     # Used for geo-location
+  field :city, :type => String        # From Geocoder location
+  field :country, :type => String     # From Geocoder location
+  field :zip_code, :type => String     # From Geocoder location
+  field :coordinates, :type => Array  # For geolocation
+  field :categories, :type => Array
+  field :status, :type => String
+  field :govt_status, :type => String
 
-  embeds_many :project_types 
-  embeds_many :project_statuses
-  embeds_many :tags 
+  embeds_many :stakeholders
   embeds_many :links
+  embeds_many :projects_fundings
 
-  has_many :project_fundings
-  has_many :stakeholders
   has_many :prints
   has_many :documents
+  has_and_belongs_to_many :tags
+
   belongs_to :user
 
-
   validates :title, :description, :address, :presence => true
-
   validates_associated :user
 
   geocoded_by :address
-  after_validation :geocode
+  after_validation :geocode_me
+
+  def geocode_me
+    result = Geocoder.search(self.address)
+    self.coordinates = result.first.coordinates
+    self.city = result.first.city
+    self.country = result.first.country
+  end
 end
