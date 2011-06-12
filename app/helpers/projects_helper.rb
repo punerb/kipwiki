0970dd88df48389417ce0a41e64de55f7d561cb0
Girish S <girish.sonawane@gmail.com>
2011-06-12 20:35:45 +0530

    added authorization for project/edit links

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,10 +1,14 @@
 module ProjectsHelper
 
-  def edit_link(field_type)
+  def edit_link(project, field_type)
     if user_signed_in?
-      link_to "Edit", "#", :class => "edit", :rel => field_type
+      if project.user == current_user
+        link_to "Edit", "#", edit_project_path(project.city, project.slug)
+      else
+        link_to "Suggest", "#", :class => "edit", :rel => field_type
+      end
     else
-      link_to "Edit", authentications_path
+      link_to "Suggest", authentications_path
     end
   end
 end
