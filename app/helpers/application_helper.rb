6252bccd9f7a565d5ad80bb6ae5afb51932db7f2
rtdp <ratnadeepdeshmane@gmail.com>
2011-06-12 22:31:56 +0530

    main menu selection helper added.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def select_if(selection, val)
     "selected" if selection == val
   end
+
+  def select_main_menu_if(first, second)
+    "selected" if first == second
+  end
 end
