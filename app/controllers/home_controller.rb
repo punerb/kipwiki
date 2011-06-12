b23d009d77b7c741226236f0ae6bf141bcbd2dc1
Swapnil <swapnil@weboniselab.com>
2011-06-12 22:31:26 +0530

    first changes to the homepage - though not done yet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,9 @@ class HomeController < ApplicationController
   layout "home_layout"
   
   def index
-    @featured_projects = Project.where({:featured => true})
-    # to be changed
-    @local_projects = Project.all
+    @featured_projects = Project.where({:featured => true})[0..2]
+    # to be changed, also this limit 3 thing has to be applied
+    @local_projects = Project.all[0..2]
   end
   
   def filter
