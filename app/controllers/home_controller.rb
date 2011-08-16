216ce660707a39eb46e457c59e6ec75dacbabf77
Pankaj Sonawane <pankajso@gmail.com>
2011-08-16 18:29:54 +0530

    Debug statements added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,9 +10,13 @@ class HomeController < ApplicationController
     @featured_projects = @shown_projects.where(:featured => true).limit(3)
     @local_projects = Project.near(@center_coords.reverse, 50, :units => :km).limit(3)  
     @coordinates = @featured_projects.collect {|project| 
+      logger.info "featured Project ID = #{project.id}"
+      logger.info "featured Project Title #{project.title}"
       c = project.coordinates.clone
       c << project.title }
     @coordinates.concat @local_projects.collect {|project| 
+      logger.info "Local Project ID = #{project.id}"
+      logger.info "Local Project Title #{project.title}"
       c = project.coordinates.clone 
       c << project.title }
   end
